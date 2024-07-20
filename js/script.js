59787ac5a2d9b258ef937e7c0c5f007d9980de45
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-07-19 19:45:30 -0500

    Router and Active Link

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const global = {
+    currentPage: window.location.pathname
+};
+
+function highlightActiveLink(e){
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+        if(link.getAttribute('href') === global.currentPage){
+            link.classList.add('active');
+        }
+    });
+}
+
+function init(){
+    switch(global.currentPage){
+        case '/':
+        case '/index.html':
+            console.log('Home');
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
+    highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
