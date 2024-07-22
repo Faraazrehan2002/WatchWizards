eda4a9bd022cb3df63c377afebce4430b2df108b
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-07-22 00:24:01 -0500

    TV Details

--- js/script.js
+++ js/script.js
@@ -127,6 +127,64 @@ async function displayMovieDetails(){
     document.querySelector('#movie-details').appendChild(div);
 }
 
+async function displayShowDetails(){
+    const showId = window.location.search.split('=')[1];
+    const show = await fetchAPIData(`tv/${showId}`);
+    displayBackgroundImage('tv', show.backdrop_path)
+    const div = document.createElement('div');
+    div.classList.add('details-top');
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+            ${show.poster_path?`<img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="${show.name}"
+                />`:
+                `<img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="${show.name}"
+                />`}
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => {
+                return `<li>${genre.name}</li>`
+              }).join(' ')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Aired Episode:</span> ${show.last_episode_to_air.name}
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+              ${show.production_companies.map((company) => {
+                return `<span>${company.name}</span>`
+              }).join(', ')}
+          </div>
+        </div>`;
+        document.querySelector('#show-details').appendChild(div);
+}
+
 async function fetchAPIData(endpoint){
     const API_KEY = '1425d005db001f914cf3740055f9de84';
     const API_URL = 'https://api.themoviedb.org/3';
@@ -153,7 +211,7 @@ function displayBackgroundImage(type, backdropPath){
     if(type === 'movie'){
         document.querySelector('#movie-details').appendChild(overlayDiv);
     }else{
-        document.querySelector('#movie-details').appendChild(overlayDiv);
+        document.querySelector('#show-details').appendChild(overlayDiv);
     }
 }
 
@@ -187,7 +245,7 @@ function init(){
             displayMovieDetails();
             break;
         case '/tv-details.html':
-            console.log('TV Details');
+            displayShowDetails();
             break;
         case '/search.html':
             console.log('Search');
